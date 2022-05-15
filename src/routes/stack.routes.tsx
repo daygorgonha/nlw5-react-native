4c4aecf47670617edf88eab91227b50ab95e78c2
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-05-15 12:30:18 -0300

    feat: added screen of selecting plant, consuming api and dealing svg

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
-import colors from '../styles/colors';
+
+
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
+
+import colors from '../styles/colors';
 
 const stackRoutes = createStackNavigator();
 
@@ -21,14 +25,19 @@ const AppRoutes: React.FC = () => (
       component={Welcome}
     />
 
-<stackRoutes.Screen 
+    <stackRoutes.Screen 
       name="UserIdentification"
       component={UserIdentification}
     />
 
-<stackRoutes.Screen 
+    <stackRoutes.Screen 
       name="Confirmation"
       component={Confirmation}
+    />
+
+    <stackRoutes.Screen 
+      name="PlantSelect"
+      component={PlantSelect}
     />
 
   </stackRoutes.Navigator>
