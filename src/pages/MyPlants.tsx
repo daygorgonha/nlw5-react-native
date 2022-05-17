abf197a58d06de92715f6f03666904cd8d270ecf
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-05-17 11:28:54 -0300

    feat: remove a plant from storage

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -4,14 +4,15 @@ import {
   View,
   Text,
   Image,
-  FlatList
+  FlatList,
+  Alert
 } from 'react-native';
 
 import { Header } from '../components/Header';
 
 import waterdrop from "../assets/waterdrop.png";
 import colors from '../styles/colors';
-import { loadPlant, PlantProps } from '../libs/storage';
+import { loadPlant, PlantProps, removePlant } from '../libs/storage';
 import { formatDistance } from 'date-fns';
 import { pt } from 'date-fns/locale';
 import fonts from '../styles/fonts';
@@ -22,6 +23,29 @@ export function MyPlants() {
   const [myPlants, setMyPlants] = useState<PlantProps[]>([]);
   const [loading,setLoading] = useState(true);
   const [nextWaterd, setNextWaterd] = useState<string>();
+
+  function handleRemove(plant: PlantProps) {
+    Alert.alert('Remover', `Deseja remover a ${plant.name}?`, [
+      {
+        text: 'Não 🙌',
+        style: 'cancel'
+      },
+      { 
+        text: 'Sim 🥲',
+        onPress: async () => {
+          try {
+            await removePlant(plant.id)
+            setMyPlants((oldData) => (
+              oldData.filter((item) => item.id !== plant.id)
+            ));
+
+          } catch (error) {
+            Alert.alert('Não foi possivel remover!😢')
+          }
+        }
+      }
+    ])
+  }
 
   useEffect(() => {
     async function loadStorageData() {
@@ -70,7 +94,10 @@ export function MyPlants() {
             data={myPlants}
             keyExtractor={(item) => String(item.id)}
             renderItem={({ item }) => (
-              <PlantCardSecondary data={item} />
+              <PlantCardSecondary 
+                data={item} 
+                handleRemove={() => {handleRemove(item)}}
+              />
             )}
             showsVerticalScrollIndicator={false}
             contentContainerStyle={{ flex: 1 }}
